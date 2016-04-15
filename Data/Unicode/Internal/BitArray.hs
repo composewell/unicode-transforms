c2d3fde5fd451f3bbedfcf7752261a89ee41125e
Harendra Kumar <harendra.kumar@gmail.com>
2016-04-15 20:31:58 +0530

    Change bitarray manipulations to 64-bit arithmetic
    The assembly is now simpler but there is no difference in performance. It seems
    the unwrapping of boxed values is breaking the processor pipelining.

--- Data/Unicode/Internal/BitArray.hs
+++ Data/Unicode/Internal/BitArray.hs
@@ -27,37 +27,32 @@ module Data.Unicode.Internal.BitArray
     , unsafeLookup
     ) where
 
-import Data.Bits ((.&.), (.|.))
-import Foreign.Storable (peekByteOff, pokeByteOff)
-import GHC.Base (Int(I#), iShiftRA#, narrow8Word#, shiftL#)
-import GHC.Word (Word8(W8#))
+import Data.Bits ((.&.), (.|.), unsafeShiftL, unsafeShiftR)
+import Foreign.Storable (peekElemOff, pokeElemOff)
+import Foreign.ForeignPtr (ForeignPtr, withForeignPtr, mallocForeignPtrArray)
+import Foreign.Marshal (fillBytes)
+import GHC.Base (Int(I#), iShiftRA#, iShiftL#)
+import GHC.Word (Word64)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Internal as I
 import qualified Data.ByteString.Unsafe as U
+import System.IO.Unsafe (unsafePerformIO)
 
 -- begin index, bytestring to store bits
 data BitArray = BitArray
   { _beg  :: {-# UNPACK #-} !Int
-  , _bits :: {-# UNPACK #-} !B.ByteString
+  , _bits :: ForeignPtr Word64
   } deriving (Eq, Ord, Show)
 
 -- | Representation of the index of a bit inside a bytestring
 -- in terms of a byte index and a bit index inside the byte
 data I = I
     {-# UNPACK #-} !Int         -- byte index
-    {-# UNPACK #-} !Word8         -- bit index
-
-shiftR :: Int -> Int -> Int
-shiftR (I# x#) (I# i#) = I# (x# `iShiftRA#` i#)
-{-# INLINE shiftR #-}
-
-shiftL :: Word8 -> Int -> Word8
-shiftL (W8# x#) (I# i#) = W8# (narrow8Word# (x# `shiftL#` i#))
-{-# INLINE shiftL #-}
+    {-# UNPACK #-} !Word64      -- bit index
 
 -- | Convert a bit index to a byte index and bit index inside the byte
 index :: Int -> I
-index i = I (i `shiftR` 3) (1 `shiftL` (i .&. 7))
+index i = I (i `unsafeShiftR` 6) (1 `unsafeShiftL` ((fromIntegral i) .&. 63) :: Word64)
 {-# INLINE index #-}
 
 -- FIXME remove the values, just keep indexes
@@ -65,30 +60,33 @@ index i = I (i `shiftR` 3) (1 `shiftL` (i .&. 7))
 setFromList :: (Int,Int) -> [(Int,Bool)] -> BitArray
 setFromList (beg, end) pairs =
     let nbits = end - beg + 1
-        nbytes = (nbits + 7) `div` 8
+        nwords = (nbits + 63) `div` 64
     in if end < beg
        then
             error "Bounds end cannot be less than begin"
-        else
-            BitArray beg $ I.unsafeCreate nbytes $ \bs -> do
-                _ <- I.memset bs 0 (fromIntegral nbytes)
-                setBits bs pairs
+        else unsafePerformIO $ do
+            fptr <- mallocForeignPtrArray nwords
+            withForeignPtr fptr $ \p -> do
+                fillBytes p 0 (nwords * 8)
+                setBits p (map fst pairs)
+                return $ BitArray beg fptr
     where
         setBits _ []      = return ()
-        setBits bs (p:ps) = do
-            let (i, _v) = p
+        setBits ptr (i:rest) = do
             if (i  < beg || i > end)
                then
                     error $ "Index " ++ show i ++ " out of range"
                 else do
-                    let I byte bit = index (i - beg)
-                    prev <- peekByteOff bs byte :: IO Word8
-                    pokeByteOff bs byte (prev .|. bit)
-                    setBits bs ps
+                    let I word bit = index (i - beg)
+                    old <- peekElemOff ptr word
+                    pokeElemOff ptr word (old .|. bit)
+                    setBits ptr rest
 
 -- | lookup a bit at a given index, does not check the bounds
 unsafeLookup :: BitArray -> Int -> Bool
-unsafeLookup !(BitArray beg bs) !i = U.unsafeIndex bs byte .&. bit /= 0
+unsafeLookup !(BitArray beg fptr) !i = unsafeIndex fptr word .&. bit /= 0
   where
-    I byte bit = index (i - beg)
+    I word bit = index (i - beg)
+    unsafeIndex ptr w = I.accursedUnutterablePerformIO $
+                            withForeignPtr ptr $ \p -> peekElemOff p w
 {-# INLINE unsafeLookup #-}
