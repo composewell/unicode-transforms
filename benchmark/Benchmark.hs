d07ad1c9ccea518dd00e183670634164ae5722ae
Harendra Kumar <harendra.kumar@gmail.com>
2016-04-16 16:16:08 +0530

    Add command line options to benchmarks

--- benchmark/Benchmark.hs
+++ benchmark/Benchmark.hs
@@ -9,22 +9,27 @@
 -- Portability : GHC
 --
 
-import           Control.DeepSeq     (NFData)
-import           Criterion.Main      (Benchmark, bench, bgroup, defaultMain, nf)
-import           Data.Text           (Text)
-import qualified Data.Text           as T
-import qualified Data.Text.ICU       as TI
-import qualified Data.Text.Normalize as UT
-import           Path                (Dir, Path, Rel, mkRelDir, toFilePath,
-                                      (</>))
-import           Path.IO             (listDir)
-import           System.FilePath     (dropExtensions, takeFileName)
+import           Control.DeepSeq           (NFData)
+import           Criterion.Main            (Benchmark, bench, bgroup,
+                                            defaultConfig, nf, runMode)
+import           Criterion.Main.Options    (describe)
+import           Data.Text                 (Text)
+import qualified Data.Text                 as T
+import qualified Data.Text.ICU             as TI
+import qualified Data.Text.Normalize       as UT
+import           Options.Applicative.Extra (execParser)
+import           Path                      (Dir, Path, Rel, mkRelDir,
+                                            toFilePath, (</>))
+import           Path.IO                   (listDir)
+import           System.FilePath           (dropExtensions, takeFileName)
 
-implementations :: [(String, Text -> Text)]
-implementations =
-    [ ("text-icu"           , TI.normalize TI.NFD)
-    , ("unicode-transforms" , UT.normalize UT.NFD)
-    ]
+textICUFuncs :: [(String, Text -> Text)]
+textICUFuncs =
+    [ ("NFD", TI.normalize TI.NFD) ]
+
+unicodeTransformFuncs :: [(String, Text -> Text)]
+unicodeTransformFuncs =
+    [ ("NFD", UT.normalize UT.NFD) ]
 
 dataDir :: Path Rel Dir
 dataDir = $(mkRelDir "benchmark") </> $(mkRelDir "data")
@@ -32,6 +37,7 @@ dataDir = $(mkRelDir "benchmark") </> $(mkRelDir "data")
 -- Truncate or expand all datasets to this size to provide a normalized
 -- measurement view across all datasets and to reduce the effect of noise
 -- because of the datasets being too small.
+dataSetSize :: Int
 dataSetSize = 1000000
 
 -- XXX read the test data fully before starting the test
@@ -45,15 +51,16 @@ getDataSet dir = do
     where
         justName  = dropExtensions . takeFileName . toFilePath
 
-benchImpl :: NFData b => (String, a -> b) -> (String, a) -> Benchmark
-benchImpl (implName, func) (dataName, txt)=
+benchAll :: NFData b => (String, a -> b) -> (String, a) -> Benchmark
+benchAll (implName, func) (dataName, txt)=
     bench (implName ++ "/" ++ dataName) (nf func txt)
 
 main :: IO ()
 main = do
+    mode    <- execParser (describe defaultConfig)
     dataSet <- getDataSet dataDir
-    defaultMain
-        [
-            bgroup "Normalize NFD" $
-                benchImpl <$> implementations <*> dataSet
+    runMode mode
+        [ bgroup "text-icu"           $ benchAll <$> textICUFuncs <*> dataSet
+        , bgroup "unicode-transforms" $ benchAll <$> unicodeTransformFuncs
+                                                 <*> dataSet
         ]
