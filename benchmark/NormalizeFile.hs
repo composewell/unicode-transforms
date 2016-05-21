d32db85fe29a7ffe4af8a6d5bca8ccb5a5eb550c
Harendra Kumar <harendra.kumar@gmail.com>
2016-05-22 02:20:10 +0530

    Add single file normalization exe for perf tests

---
+++ benchmark/NormalizeFile.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+-- |
+-- Copyright   : (c) 2016 Harendra Kumar
+--
+-- License     : BSD-style
+-- Maintainer  : harendra.kumar@gmail.com
+-- Stability   : experimental
+-- Portability : GHC
+--
+
+import           Control.DeepSeq           (deepseq)
+import qualified Data.Text.NormalizeNative as UT
+import           System.Environment        (getArgs)
+
+-- Truncate or expand all datasets to this size to provide a normalized
+-- measurement view across all datasets and to reduce the effect of noise
+-- because of the datasets being too small.
+dataSetSize :: Int
+dataSetSize = 1000000
+
+strInput :: FilePath -> IO String
+strInput file = fmap (take dataSetSize . cycle) (readFile file)
+
+main :: IO ()
+main = do
+    [file] <- getArgs
+    input <- strInput file
+    UT.normalize UT.NFD input `deepseq` return ()
