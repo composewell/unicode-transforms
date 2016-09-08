79a5112c021293c340487e344c296e774c6b504b
Harendra Kumar <harendra.kumar@gmail.com>
2016-09-09 00:40:27 +0530

    Implement bytestring normalization in terms of text

--- Data/ByteString/UTF8/Normalize.hs
+++ Data/ByteString/UTF8/Normalize.hs
@@ -17,29 +17,14 @@ module Data.ByteString.UTF8.Normalize
     , normalize
     ) where
 
-import           Data.ByteString        (ByteString)
-import           Data.ByteString.Unsafe (unsafePackMallocCStringLen,
-                                         unsafeUseAsCStringLen)
-import           Data.Unicode.Types
-import           Data.Unicode.UTF8Proc
-import           System.IO.Unsafe       (unsafePerformIO)
+import           Data.ByteString     (ByteString)
+import           Data.Text.Encoding  (decodeUtf8, encodeUtf8)
+import           Data.Unicode.Types  (NormalizationMode (..))
+import qualified Data.Text.Normalize as T
+
+-- This is now simply a wrapper over Text normalization
 
 -- | Perform Unicode normalization on a UTF8 encoded @ByteString@ according to
 -- the specified normalization mode.
 normalize :: NormalizationMode -> ByteString -> ByteString
-normalize mode bs =
-    case mode of
-         NFD  -> tr Decomposed []
-         NFKD -> tr Decomposed [Compat]
-         NFC  -> tr Composed   []
-         NFKC -> tr Composed   [Compat]
-    where
-        tr nf opts = transformBS [StableMode]
-                                 [Normalize (mkNormalizeOp nf opts)]
-                                 bs
-
-transformBS :: [TransformMode] -> [TransformOp] -> ByteString -> ByteString
-transformBS modes ops bs =
-    let transform2bs str =     transform modes ops str
-                           >>= unsafePackMallocCStringLen
-    in unsafePerformIO $ unsafeUseAsCStringLen bs transform2bs
+normalize mode = (encodeUtf8 . T.normalize mode . decodeUtf8)
