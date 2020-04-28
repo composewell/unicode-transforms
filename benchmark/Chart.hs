7b04baa8e92a4d24ccc5cb2c33b346a63cceb0b2
Harendra Kumar <harendra@composewell.com>
2020-04-28 11:42:52 +0530

    Include all benchmarks
    Remove obsolete benchmarks

--- benchmark/Chart.hs
+++ benchmark/Chart.hs
@@ -11,37 +11,18 @@ selectBench :: (SortColumn -> Either String [(String, Double)]) -> [String]
 selectBench f =
     -- reverse
       fmap fst
-    $ either error (sortOn snd) $ f $ ColumnIndex 0
+    $ either error (sortOn fst) $ f $ ColumnIndex 0
 
 main :: IO ()
 main = do
-    let icu = "Normalize NFD/text-icu/"
-    let utrans = "Normalize NFD/unicode-transforms/"
-
-    let icu1 = "text-icu/NFD/"
-    let utrans1 = "unicode-transforms/NFD/"
-    let utf8proc1 = "utf8proc/NFD/"
-
-    let utrans2 = "unicode-transforms-text/NFD/"
-
     let cfg = defaultConfig
             { classifyBenchmark = \bs ->
-                case stripPrefix icu bs of
+                case stripPrefix "text-icu/" bs of
                     Just x -> Just ("ICU", x)
                     Nothing ->
-                        case stripPrefix icu1 bs of
-                            Just x -> Just ("ICU", x)
-                            Nothing ->
-                                case stripPrefix utrans bs of
-                                    Just x -> Just ("unicode-transforms",x)
-                                    Nothing ->
-                                        case stripPrefix utrans1 bs of
-                                            Just x -> Just ("unicode-transforms",x)
-                                            Nothing ->
-                                                case stripPrefix utrans2 bs of
-                                                    Just x -> Just ("unicode-transforms",x)
-                                                    Nothing ->
-                                                        fmap ("utf8proc",) $ stripPrefix utf8proc1 bs
+                        case stripPrefix "unicode-transforms-text/" bs of
+                            Just x -> Just ("unicode-transforms",x)
+                            Nothing -> error "unknown benchmark"
             , selectBenchmarks = selectBench
             , presentation = Groups PercentDiff
             }
