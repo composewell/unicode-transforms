a48b3b585c3545dff8f051496996b2ed6d9c6b57
Harendra Kumar <harendra@composewell.com>
2020-07-28 23:40:53 +0530

    Make some tweaks to chart generation
    1) use new version of bench-show
    2) use unicode-transforms as first column when comparing with ICU
    3) sort based on percent diff

--- benchmark/Chart.hs
+++ benchmark/Chart.hs
@@ -7,11 +7,18 @@ module Main where
 import BenchShow
 import Data.List
 
-selectBench :: (SortColumn -> Either String [(String, Double)]) -> [String]
+selectBench
+    :: (SortColumn -> Maybe GroupStyle -> Either String [(String, Double)])
+    -> [String]
 selectBench f =
-    -- reverse
-      fmap fst
-    $ either error (sortOn fst) $ f $ ColumnIndex 0
+      reverse
+    $ fmap fst
+    $ either
+        (const
+            $ either error (sortOn snd)
+            $ f (ColumnIndex 0) (Just PercentDiff))
+        (sortOn snd)
+    $ f (ColumnIndex 1) (Just PercentDiff)
 
 main :: IO ()
 main = do
@@ -24,7 +31,13 @@ main = do
                             Just x -> Just ("unicode-transforms",x)
                             Nothing -> error "unknown benchmark"
             , selectBenchmarks = selectBench
-            , presentation = Groups PercentDiff
+            , selectGroups = \gs ->
+                filterGroup "unicode-transforms" gs ++ filterGroup "ICU" gs
+            , presentation = Groups Absolute
             }
     -- graph "results.csv" "unicode-graph" cfg
     report "results.csv" Nothing cfg
+
+    where
+
+    filterGroup grp gs = filter (\(name,_) -> name == grp) gs
